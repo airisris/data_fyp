5834ada9d2b801cd7389a9d68cba9a216b57d708
Airis Alesya <airisalesya0911@gmail.com>
2026-01-01 17:56:55 +0800

    day7-commit

--- .ipynb_checkpoints/pill_app-checkpoint.py
+++ .ipynb_checkpoints/pill_app-checkpoint.py
@@ -5,11 +5,24 @@ import numpy as np
 from PIL import Image
 
 model = tensorflow.keras.models.load_model('pill.keras')
+class_names = ['Alaxan', 'Bactidol', 'Bioflu', 'Biogesic', 'DayZinc', 'Decolgen', 'Fish Oil', 'Kremil S', 
+               'Medicol', 'Neozep']
 
-st.title('Pill Classification App')
+st.title('Pill Classification App💊')
 
 uploaded_file = st.file_uploader("Upload an image of a pill...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image")
+
+    # if st.button('Classify Pill'):
+    resized = image.resize((150, 150))
+    input_arr = np.array(resized)
+    rescaled = input_arr / 255
+    final_img = np.expand_dims(rescaled, axis=0)
+        
+    prediction = model.predict(final_img)
+    class_index = np.argmax(prediction[0]) # ai-generated
+    confidence = prediction[0][class_index] * 100
+    st.success(f"Predicted Pill: {class_names[class_index]} with {confidence:.2f}% confidence")
